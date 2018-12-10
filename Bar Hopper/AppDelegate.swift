31cf124e4b0f4165329f2f4c04838ffa2b4faf27
Step <cannuscs@bc.edu>
2018-12-10 14:42:45 -0500

    download pod files

--- Bar Hopper/AppDelegate.swift
+++ Bar Hopper/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        GMSPlacesClient.provideAPIKey("AIzaSyD4EuMTEIxOstsgZa1QDsa_LK-9s_N0IqA")
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
---
+++ Bar Hopper/Bar.swift
@@ -0,0 +1,9 @@
+//
+//  Bar.swift
+//  Bar Hopper
+//
+//  Created by Step Cannuscio on 12/10/18.
+//  Copyright © 2018 Step Cannuscio. All rights reserved.
+//
+
+import Foundation
